872280dd8ec890669b6e40af124422ab80bedda0
ChristopherLMiller <chris@christopherleemiller.me>
2023-10-12 01:58:10 -0400

    feat: :loud_sound: Implement per request logging

--- apps/api/src/app/global.module.ts
+++ apps/api/src/app/global.module.ts
@@ -1,5 +1,5 @@
 import { CacheInterceptor, CacheModule } from '@nestjs/cache-manager';
-import { Logger, Module } from '@nestjs/common';
+import { Logger, MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
 import { DevtoolsModule } from '@nestjs/devtools-integration';
@@ -8,6 +8,7 @@ import { redisStore } from 'cache-manager-redis-yet';
 import { config } from '../../config';
 import { supabaseAuthGuard } from '../common/guards/supabaseAuth.guard';
 import { UserInterceptor } from '../common/interceptors/user.interceptor';
+import { LoggerMiddleware } from '../common/middleware/logging.middleware';
 import { AuthModule } from './common/auth/auth.module';
 import { PrismaModule } from './common/prisma/prisma.module';
 import { SettingsModule } from './common/settings/settings.module';
@@ -83,4 +84,8 @@ import { V1Module } from './v1/v1.module';
     Logger,
   ],
 })
-export class GlobalModule {}
+export class GlobalModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
--- apps/api/src/common/middleware/logging.middleware.ts
+++ apps/api/src/common/middleware/logging.middleware.ts
@@ -1,12 +1,24 @@
-import { Logger } from '@nestjs/common';
+import { Injectable, Logger, NestMiddleware } from '@nestjs/common';
 import { NextFunction, Request, Response } from 'express';
 
-export function logger(req: Request, res: Response, next: NextFunction) {
-  // ignore the 'health-check' route as its just render checking app availablility
-  if (req.originalUrl !== '/api/v1/health') {
-    Logger.log(
-      `Request: ${req.hostname}, URL: ${req.originalUrl}; HTTP Code: ${res.statusCode}`
+@Injectable()
+export class LoggerMiddleware implements NestMiddleware {
+  private logger = new Logger('HTTP');
+  use(req: Request, res: Response, next: NextFunction) {
+    const startTime = Date.now();
+    const remoteIP = req.ip;
+
+    // call the next function before finishing off, for timing
+    next();
+
+    const responseCode = res.statusCode;
+    const endTime = Date.now();
+    const totalTime = endTime - startTime;
+
+    this.logger.log(
+      `${totalTime < 30 ? '✅' : '❌'} Req: ${
+        req.originalUrl
+      }: Remote IP: ${remoteIP} Res: ${responseCode} - Total Time: ${totalTime}ms`,
     );
   }
-  next();
 }

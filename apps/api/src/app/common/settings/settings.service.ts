1f239a5786d8ad5c710bf32615213140fab15c60
ChristopherLMiller <chris@christopherleemiller.me>
2023-10-31 04:10:19 -0400

    refactor: Bring settings up to new standards

--- apps/api/src/app/common/settings/settings.service.ts
+++ apps/api/src/app/common/settings/settings.service.ts
@@ -4,7 +4,7 @@ import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class SettingsService {
-  logger = new Logger(SettingsService.name);
+  private _logger = new Logger(SettingsService.name);
 
   constructor(private prisma: PrismaService) {}
 
@@ -14,8 +14,17 @@ export class SettingsService {
     return data.value;
   }
 
-  async getField<T>(key: string, field): Promise<T> {
+  // @deprecated use getField instead
+  async getFieldOld<T>(key: string, field): Promise<T> {
     const data = await this.getSetting(key);
     return data[field];
+  }
+
+  async getField<T>(key: string, field: string): Promise<T> {
+    return (await this.getSetting(key))['fields'][field];
+  }
+
+  async getFieldValue<T>(key: string, field: string): Promise<T> {
+    return (await this.getSetting(key))['fields'][field].value;
   }
 }

aa161eced024e3d1e5f9fe9c6cfe1bd457d4467f
ChristopherLMiller <chris@christopherleemiller.me>
2023-12-02 02:19:07 -0500

    fix: :loud_sound: API - IgnoreLogging decorator works
    API - Helps to actually make the IgnoreLogging decorator work

--- apps/api/src/app/v1/health/health.controller.ts
+++ apps/api/src/app/v1/health/health.controller.ts
@@ -1,14 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 import { ApiOperation } from '@nestjs/swagger';
 import { PermissionsPublic } from '../../../common/decorators/auth.decorator';
-import { NoLogging } from '../../../common/decorators/logging.decorator';
+import { IgnoreLogging } from '../../../common/decorators/logging.decorator';
 
 @Controller({ version: '1', path: 'health' })
 export class HealthController {
   @Get('/status')
   @ApiOperation({ description: 'Get the stauts of the server' })
   @PermissionsPublic()
-  @NoLogging()
+  @IgnoreLogging()
   getAppStatus() {
     const healthcheck = {
       uptime: process.uptime(),

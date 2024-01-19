b3b6b3e85aa5c2549cfd82569ebb54242ed469e8
ChristopherLMiller <chris@christopherleemiller.me>
2024-01-19 04:38:32 -0500

    feat: :stethoscope: API - Further healtchecking abilities

--- apps/api/src/app/common/health/health.controller.ts
+++ apps/api/src/app/common/health/health.controller.ts
@@ -1,9 +1,25 @@
 import { Controller, Get } from "@nestjs/common";
 import { ApiOperation } from "@nestjs/swagger";
+import {
+  HealthCheck,
+  HealthCheckService,
+  HttpHealthIndicator,
+  MemoryHealthIndicator,
+  PrismaHealthIndicator,
+} from "@nestjs/terminus";
 import { IgnoreLogging } from "../../../common/decorators/logging.decorator";
+import { PrismaService } from "../prisma/prisma.service";
 
 @Controller({ path: "health" })
 export class HealthController {
+  constructor(
+    private health: HealthCheckService,
+    private http: HttpHealthIndicator,
+    private memory: MemoryHealthIndicator,
+    private prismaHealth: PrismaHealthIndicator,
+    private prisma: PrismaService,
+  ) {}
+
   @Get("/status")
   @ApiOperation({ description: "Get the stauts of the server" })
   @IgnoreLogging()
@@ -16,5 +32,15 @@ export class HealthController {
     };
 
     return { data: healthcheck };
+  }
+
+  @Get()
+  @HealthCheck()
+  check() {
+    return this.health.check([
+      () => this.http.pingCheck("nestjs-docs", "https://docs.nestjs.com"),
+      () => this.memory.checkHeap("memory_heap", 158 * 1024 * 1024),
+      () => this.prismaHealth.pingCheck("prisma", this.prisma),
+    ]);
   }
 }
